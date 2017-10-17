add5eef4a7b48807b45258c9563e99ca7f3cebee
Daniel Koch <dankodak@live.de>
2017-10-17 12:23:25 +0200

    Hello World

--- Blatt1/Aufgabe5.py
+++ Blatt1/Aufgabe5.py
@@ -1,2 +1,8 @@
-a,b,c = 1,2,3
-print(a)
+import math
+import numpy as np
+
+n=input("n: ")
+interval = np.linspace(-0.5,0.5,n)
+print("{:^10}|{:^10}|{:^10}".format("x","sin(x)","Fehler"))
+for i in interval:
+    print("{:^10.4f}|{:^10.4f}|{:^10.4f}".format(i,math.sin(i),math.fabs(i-math.sin(i))))
